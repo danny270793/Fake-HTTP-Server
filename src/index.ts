b9ee42f99479000ba473d76841b53f87ceb6c460
Danny Vaca <danny270793@gmail.com>
2023-05-01 20:48:04 -0500

    show number of requests recieved

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import Os from 'os'
 
 DotEnv.config()
 
+let requestsReceived: number = 0
+
 const server = Http.createServer((request, response) => {
+  requestsReceived += 1
   const hostname: string = Os.hostname()
   response.writeHead(200)
-  response.end(`<html><head><title>HTTP Hello World</title></head><body><h1>Hello from ${hostname}</h1><p>Running on server ${process.env.SERVER_NAME ?? 'NO NAME PROVIDED'}</p></body></html`)
+  response.end(`<html><head><title>HTTP Hello World</title></head><body><h1>Hello from ${hostname}</h1><p>Running on server ${process.env.SERVER_NAME ?? 'NO NAME PROVIDED'}</p><p>${requestsReceived} requests received</p></body></html`)
 })
 
 const port: number = 3000
